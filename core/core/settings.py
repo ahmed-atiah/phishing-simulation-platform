9e7125be87172b07aa3b9aaa0ae1ee8e743bfd86
muhamnmed17 <nur554413@gmail.com>
2026-05-12 13:55:14 +0300

    fix csrf

--- core/core/settings.py
+++ core/core/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://127.0.0.1:8000', cast=Csv())
 
 
 # Application definition
